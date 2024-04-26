425fd08d9642775b9d03830c7c1897e2fa0ffa08
Atis <a.saule@inbox.lv>
2024-04-26 12:05:13 +0300

    adding flask rest api

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from configuration.file_configuration import FileConfiguration
 from weather_service.weather_service import WeatherService
 from electricity_price_service.electricity_price_service import ElectricityPriceService
 from switch_service.switch_service import SwitchService
+from rest_api.flask_rest_api import FlaskRESTAPI
 
 configuration = FileConfiguration()
 
@@ -17,7 +18,17 @@ electricity_price_service = ElectricityPriceService(electricity_price_api, elect
 weather_service = WeatherService(weather_api, weather_processor, repository_service)
 switch_service = SwitchService(configuration, weather_service, electricity_price_service)
 
-weather_service.regenerate_weather_data(*location_service.get_location())
-electricity_price_service.regenerate_electricity_price_data()
-status = switch_service.get_switch_status('boiler01')
-print(status)
+import sys
+import os
+
+print("Current Path:", os.getcwd())
+print("Python Executable:", sys.executable)
+print("System Path:", sys.path)
+
+rest_api = FlaskRESTAPI(switch_service, configuration)
+rest_api.run_app()
+
+# weather_service.regenerate_weather_data(*location_service.get_location())
+# electricity_price_service.regenerate_electricity_price_data()
+# status = switch_service.get_switch_status('boiler01')
+# print(status)
---
+++ src/rest_api/flask_rest_api.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_httpauth import HTTPBasicAuth
+
+
+class FlaskRESTAPI:
+    def __init__(self, switch_service, configuration):
+        self.switch_service = switch_service
+        self.configuration = configuration
+        self.app = Flask('__name__')
+        self.auth = HTTPBasicAuth()
+        self.setup_routes()
+        self.username = configuration.get('rest_username')
+        self.password = configuration.get('rest_password')
+
+
+    def run_app(self):
+        self.app.run(debug=True)
+
+    def setup_routes(self):
+
+        @self.auth.verify_password
+        def verify_password(username, password):
+            if username == self.username and password == self.password:
+                return username
+
+        @self.app.route('/status')
+        @self.auth.login_required
+        def get_switch_status():
+            switch_name = request.args.get('name')
+            return self.switch_service.get_switch_status(switch_name)
