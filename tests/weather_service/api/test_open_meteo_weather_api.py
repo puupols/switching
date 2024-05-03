ee3e5b5787cac976ef7b3c3aae6ca60d559c40c5
Atis <a.saule@inbox.lv>
2024-05-03 19:23:18 +0300

    tests for open meteo weather api

---
+++ tests/weather_service/api/test_open_meteo_weather_api.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import Mock, patch
+from src.weather_service.api.open_meteo_weather_api import OpenMeteoWeatherAPI
+
+class TestOpenMeteoWeatherAPI(unittest.TestCase):
+
+    @patch('src.weather_service.api.open_meteo_weather_api.requests')
+    def test_get_weather_data(self, mock_requests):
+
+        # Setup
+        data = {"my": "json", "file": "here"}
+        response = Mock()
+        response.json.return_value = data
+        mock_requests.get.return_value = response
+        mock_configuration = Mock()
+        mock_configuration.get.side_effect = lambda key: {
+            'open_meteo_url': 'https://api.openmeteo.com?latitude={latitude}&longitude={longitude}&weather_data_types={weather_data_types}&forecast_days={forecast_days}&timezone={timezone}'}[
+            key]
+        api_service = OpenMeteoWeatherAPI(mock_configuration)
+
+        # Action
+        result = api_service.get_weather_data(52.555, 24.5555)
+
+        # Assertion
+        expected_url = 'https://api.openmeteo.com?latitude=52.555&longitude=24.5555&weather_data_types=cloud_cover,temperature,sunshine_duration&forecast_days=3&timezone=EET'
+        mock_requests.get.assert_called_once_with(expected_url)
+        response.json.assert_called_once()
+        self.assertEqual(result, data)
+
